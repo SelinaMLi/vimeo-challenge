f909531314dfcad4b071800c3a6471ff5f4b776e
Selina Li <selinali1129@gmail.com>
2018-12-04 01:05:09 -0500

    responsive layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="row Content_1">
+          <div className="text-1 col-md-12 order-md-1 col-lg-6 order-lg-2">
+            <h2>MONSOON III</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              tincidunt ipsum augue. In faucibus vehicula magna pulvinar
+              aliquam. Cras aliquam feugiat lorem non auctor. Quisque sed lorem
+              egestas mauris venenatis commodo eu id nibh. Ut porta libero sed
+              semper faucibus.
+            </p>
+          </div>
+          <div className="col-md-12 order-md-2 col-lg-6 order-lg-1">
+            <img
+              src="https://i.vimeocdn.com/video/595198868_505x160.jpg"
+              className="img-fluid"
+            />
+          </div>
+        </div>
+        <div className="row Content_2">
+          <div className="text-2 col-lg-6">
+            <h3>BEAMS</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              tincidunt ipsum augue. In faucibus vehicula magna pulvinar
+              aliquam. Cras aliquam feugiat lorem non auctor. Quisque sed lorem
+              egestas mauris venenatis commodo eu id nibh. Ut porta libero sed
+              semper faucibus.
+            </p>
+          </div>
+          <div className="col-lg-6">
+            <img
+              src="https://i.vimeocdn.com/video/589972810_530x315.jpg"
+              className="img-fluid"
+            />
+          </div>
+        </div>
+        <div className="row Content_3">
+          <div className="text-1 col-md-12 order-md-1 col-lg-6 order-lg-2">
+            <h3>MOVE 2</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              tincidunt ipsum augue. In faucibus vehicula magna pulvinar
+              aliquam. Cras aliquam feugiat lorem non auctor. Quisque sed lorem
+              egestas mauris venenatis commodo eu id nibh. Ut porta libero sed
+              semper faucibus.
+            </p>
+          </div>
+
+          <div className="col-md-12 order-md-2 col-lg-6 order-lg-1">
+            <img
+              src="https://i.vimeocdn.com/video/590587169_530x315.jpg"
+              className="img-fluid"
+            />
+          </div>
+        </div>
       </div>
     );
   }
